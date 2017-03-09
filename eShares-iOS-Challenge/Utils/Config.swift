d48e61e79c568e6a61cc060eaabe45a39358a7fd
Thiago Orniz Martin <thiago.orniz@gmail.com>
2017-03-09 19:49:53 -0300

    organise files into folders

---
+++ eShares-iOS-Challenge/Utils/Config.swift
@@ -0,0 +1,16 @@
+//
+//  Config.swift
+//  eShares-iOS-Challenge
+//
+//  Created by Thiago Orniz Martin on 07/03/17.
+//  Copyright © 2017 Thiago Orniz Martin. All rights reserved.
+//
+
+import Foundation
+
+struct Config {
+    
+    static let clientID: String = "33247fcbd9c94646b56f5b4ac3078aa0"
+    static let clientSecret: String = "64e0cf96ba964276b66fd9fb28a71e1c"
+    
+}
